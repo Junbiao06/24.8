898d8424442868281b929bb18787a19d21a6b388
Junbiao06 <2675661022@qq.com>
2024-08-11 15:01:53 +0800

    Add files via upload

---
+++ test24.8.10/compare_questions/1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 20;
+    if(a>b)
+    {
+        printf("a is greater than b");
+    }
+    else if(a<b)
+    {
+        printf("a is less than b");
+    }
+    else
+    {
+        printf("a is equal to b");
+    }
+
+}
