508ebd1c8f0a118fb54177ba18e95503f0dc0f12
Junbiao06 <2675661022@qq.com>
2024-08-12 11:16:29 +0800

    Add files via upload

---
+++ test24.8.11/python_input.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 12 10:46:45 2024
+
+@author: 26756
+"""
+
+A=input("Please input integers: ") #一个字符串
+print(A)
+print(type(A))
+B,C,D=(input("please input 3 integers: ").split()) #均为列表
+print(B,C,D)
+print(type(B))
+E,F,G=map(int,input("Please input 3 integers: ").split())
+print(E,F,G)
+print(type(E))
